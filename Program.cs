9d4074a6ee9e87a0871e90383fcd9316e5a90177
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-04-28 05:39:20 -0300

    Add .editorconfig + editorconfig fixes

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using FServ;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+      services.AddHostedService<Worker>();
     })
     .Build();
 
--- Worker.cs
+++ Worker.cs
@@ -2,19 +2,19 @@ namespace FServ;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+  private readonly ILogger<Worker> _logger;
 
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+  public Worker(ILogger<Worker> logger)
+  {
+    _logger = logger;
+  }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+  protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    while (!stoppingToken.IsCancellationRequested)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
-        }
+      _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+      await Task.Delay(1000, stoppingToken);
     }
+  }
 }
